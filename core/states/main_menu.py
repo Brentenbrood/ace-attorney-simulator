b988821eaf8f9d08ace740b2ea68dd318c31bd0d
Brent Minkes <brentminkes@hotmail.com>
2015-05-10 15:29:13 +0200

    connect wiimote

--- core/states/main_menu.py
+++ core/states/main_menu.py
@@ -3,11 +3,17 @@ import cwiid
 from ..utils.colors import Color
 
 class Menu():
-    def __init__(self, screen):
+    def __init__(self, screen, wm):
+
     	self.screen = screen
         self.myfont = pygame.font.SysFont("monospace", 16)
         self.wiimotetext = self.myfont.render("Press 1 + 2 on the Wii Remote", 1, Color.GREEN)
         self.screen.blit(self.wiimotetext, (screen.get_width()/2-130, 5))
+
+        self.wm = wm
+        self.wm.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_ACC
+        self.wm.led = 1
+        self.screen.blit("Wii Remote connected", (screen.get_width()/2-130, 5), 5)
 
     def updated(self):
     	pass
