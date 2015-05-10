00feefdb588ed91a2888f4c25104a7418aeb4db6
Alexander Freeman <rubberduck313@gmail.com>
2015-05-10 14:34:00 +0200

    Continued refactoring

---
+++ game.py
@@ -0,0 +1,28 @@
+import pygame
+import states
+
+class Game():
+	def __init__(self):
+		pygame.init()
+    	self.size = (800, 500)
+    	self.screen = pygame.display.set_mode(self.size)
+    	self.clock = pygame.time.Clock()
+    	pygame.display.set_caption("Ace Attorney Simulator")
+    	self.font = pygame.font.SysFont("monospace", 16)
+    	self.state = None
+    	self.running = False
+
+	def start(self):
+		self.running = True
+
+		while self.running:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					running = False
+			
+			#self.state.update()
+			#self.state.draw()
+
+			pygame.display.flip()
+			self.clock.tick(60)
+
---
+++ start.py
@@ -0,0 +1,5 @@
+import game
+
+if __name__ == "__main__":
+	game = Game.game()
+	game.start()
