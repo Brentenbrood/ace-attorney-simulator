314618fb725707d9ba31bfa9a398e97fc600cbcf
Brent Minkes <brentminkes@hotmail.com>
2015-05-10 16:33:42 +0200

    reformat + not working state switch (lol)

--- core/game.py
+++ core/game.py
@@ -3,32 +3,34 @@ import cwiid
 import time
 from states import main_menu
 
+
 class Game():
-	def __init__(self):
-		pygame.init()
-		self.size = (800, 500)
-		self.screen = pygame.display.set_mode(self.size)
-		self.clock = pygame.time.Clock()
-		pygame.display.set_caption("Ace Attorney Simulator")
-		self.font = pygame.font.SysFont("monospace", 16)
+    def __init__(self):
+        pygame.init()
+        self.size = (800, 500)
+        self.screen = pygame.display.set_mode(self.size)
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption("Ace Attorney Simulator")
+        self.font = pygame.font.SysFont("monospace", 16)
 
-		self.screen.blit(self.font.render("Press 1 + 2 on the wiimote", 1, (255,255,255)), (self.screen.get_width()/2-130, 5))
-		pygame.display.update()
-		
-		self.state = main_menu.Menu(self.screen, cwiid.Wiimote())
-		self.running = False
+        self.screen.blit(self.font.render("Press 1 + 2 on the wiimote", 1, (255, 255, 255)),
+                         (self.screen.get_width() / 2 - 130, 5))
+        pygame.display.update()
 
-	def start(self):
-		self.running = True
+        self.state = main_menu.Menu('game', self.screen, cwiid.Wiimote())
+        self.running = False
 
-		while self.running:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					self.running = False
+    def start(self):
+        self.running = True
 
-			#self.state.update()
-			#self.state.draw()
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
 
-			pygame.display.update()
-			self.clock.tick(60)
+                    # self.state.update()
+            #self.state.draw()
+
+            pygame.display.update()
+            self.clock.tick(60)
 
--- core/states/main_menu.py
+++ core/states/main_menu.py
@@ -3,9 +3,9 @@ import cwiid
 from ..utils.colors import Color
 
 class Menu():
-    def __init__(self, screen, wm):
+    def __init__(self, game, screen, wm):
     	self.screen = screen
-        self.myfont = pygame.font.SysFont("monospace", 16)
+        self.font = pygame.font.SysFont("monospace", 16)
 
         self.wm = wm
         self.wm.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_ACC
@@ -14,9 +14,12 @@ class Menu():
 
         #Reset the screen
         pygame.draw.rect(self.screen, (0,0,0), (0,0,self.screen.get_width(), self.screen.get_height()))
+        self.wiimotetext = self.font.render("Press A to continue", 1, Color.GREEN)
+        self.screen.blit(self.wiimotetext, (300, 5))
 
     def update(self):
-    	pass
+    	while wm.state['buttons'] == 8:
+            self.state = game_state.GameState(self.screen, cwiid.Wiimote())
 
     def draw(self):
     	pass
