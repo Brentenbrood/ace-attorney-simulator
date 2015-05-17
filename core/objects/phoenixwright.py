46bcc9676435cebc2bc250a9c0db016b18f9fc31
Brent Minkes <brentminkes@hotmail.com>
2015-05-17 14:07:03 +0200

    sprite and sound added

--- core/objects/phoenixwright.py
+++ core/objects/phoenixwright.py
@@ -6,21 +6,21 @@ from core.utils.GIFImage import GIFImage
 from core.objects.person import Person
 
 class Lawyer(Person):
-    def __init__(self, x, y, base_sprite_path):
-        super(Lawyer, self).__init__(x, y, base_sprite_path)
+	def __init__(self, x, y, base_sprite_path):
+		super(Lawyer, self).__init__(x, y, base_sprite_path)
 
-        self.addSound("objection", "../sound/sfx-objection.wav")
-        self.addSound("deskslam", "../sound/sfx-deskslam2.wav")
+		self.addSound("objection", "../sound/sfx-objection.wav")
+		self.addSound("deskslam", "../sound/sfx-deskslam2.wav")
 
-        self.addState(AnimState.normal, "-normal.gif", False)
-        self.addState(AnimState.deskslam, "-deskslam.gif", True)
-        self.addState(AnimState.objection, "-objection.gif", True)
-        self.addState(AnimState.paperslap, "-paperslap.gif", False)
+		self.addState(AnimState.normal, "-normal.gif", False)
+		self.addState(AnimState.deskslam, "-deskslam.gif", True)
+		self.addState(AnimState.objection, "-objection.gif", True)
+		self.addState(AnimState.paperslap, "-paperslap.gif", False)
 
-        self.changeState(AnimState.normal)
-
-
+		self.changeState(AnimState.normal)
 
 
 
-        
+
+
+		
